5c02877dee4e194328b16d189eb3ea2c400f58f6
Andrew <andrewjihoonc@gmail.com>
2016-10-12 20:36:27 +0700

    Adding some default content

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,1 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Photo.create([{ username: 'Star Wars' }, { caption: 'Lord of the Rings' }, { likes_count: 10000}, { url: "https://lh5.googleusercontent.com/-rotwX2rMVUM/AAAAAAAAAAI/AAAAAAABb28/WRGFcZkDV6Q/s0-c-k-no-ns/photo.jpg"}])
